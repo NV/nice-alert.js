604cfe1830f156910163700d4ad03909ed0d8df5
Nikita Vasilyev <me@elv1s.ru>
2009-09-24 14:20:04 +0400

    tabs to spaces added some ;

--- nice_alert.user.js
+++ nice_alert.user.js
@@ -3,8 +3,8 @@
 // @namespace   http://userscripts.ru/
 // @include     *
 // @description Not modal alert box
-// @author      Nikita Vasilev
-// @version     0.6
+// @author      Nikita Vasilyev
+// @version     0.7
 // @licence     GPL
 // ==/UserScript==
 
@@ -14,102 +14,102 @@
 var w = window.wrappedJSObject || window;
 
 if (!w.alert.isNice) {
-		
-	w.alert = function(msg){
 
-		if (typeof GM_addStyle == 'undefined') {
-			/**
-			 * @param css String like '* {color:red}'
-			 */
-			function GM_addStyle(css) {
-				var head = document.getElementsByTagName('head')[0];
-				if (head) {
-					var style = document.createElement("style");
-					style.type = "text/css";
-					style.appendChild(document.createTextNode(css));
-					head.appendChild(style);
-				}
-			}
-		}
+  w.alert = function(msg){
 
-		GM_addStyle('#nice_alert {\
-			color: InfoText;\
-			font: 14px/16px sans-serif;\
-			position: fixed;\
-			top: 0;\
-			right: 0;\
-			margin: 0;\
-			padding: 0;\
-			list-style-type: none;\
-			float: left;\
-			cursor: pointer;\
-			text-align: left;\
-			z-index: 99;\
-		}\
-		#nice_alert li {\
-			background-color: InfoBackground;\
-			border-bottom: 1px solid #ccc;\
-			border-bottom: 1px solid rgba(0,0,0,0.3);\
-			margin: 0;\
-			opacity: 0;\
-			float: right;\
-			clear: both;\
-			overflow: hidden;\
-			font-size: 14px;\
-			outline: 0;\
-			-webkit-box-shadow: 0px 2px 8px rgba(0,0,0,0.2);\
-			-moz-box-shadow: 0px 2px 8px rgba(0,0,0,0.3);\
-		}');
+    if (typeof GM_addStyle == 'undefined') {
+      /**
+       * @param css String like '* {color:red}'
+       */
+      function GM_addStyle(css) {
+        var head = document.getElementsByTagName('head')[0];
+        if (head) {
+          var style = document.createElement("style");
+          style.type = "text/css";
+          style.appendChild(document.createTextNode(css));
+          head.appendChild(style);
+        }
+      }
+    }
 
-		var niceAlert = document.createElement('ol');
-		niceAlert.id = 'nice_alert';
-	
-		if (document.body) {
-			document.body.appendChild(niceAlert);
-			niceAlert.addEventListener('click',function(e){
-				var t = e.target //@cc_on || window.event.srcElement;
-				if (t.tagName == 'LI') {
-					var h = t.clientHeight - 18;
-					t.style.height = h +'px';
-					var closing = setInterval(function(){
-						t.style.opacity -= 0.1;
-						//t.style.fontSize = parseInt(t.style.fontSize) - 1 +'px';
-						t.style.paddingTop = parseInt(t.style.paddingTop) - 1 +'px';
-						t.style.paddingBottom = parseInt(t.style.paddingBottom) - 1 +'px';
-						var currentHeight = parseInt(t.style.height) - h/10;
-						t.style.height = (currentHeight < 0 ? 0 : currentHeight) +'px';
-						if (t.style.opacity <= 0) {
-							t.style.display = 'none';
-							clearInterval(closing);
-						}
-					}, 30)
-				}
-			}, false)
-		}
-	
-		var cache = document.createElement('li');
-		cache.style.padding = '0px 16px';
-		cache.tabIndex = 0;
-		
-		(w.alert = function(msg){
-			w.alert.isNice = 'Of couse it is!';
-			//if (niceAlert.childNodes.length > 99) return
-			var li = cache.cloneNode(false);
-			li.appendChild(document.createTextNode(msg));
-			niceAlert.appendChild(li);
-			var showing = setInterval(function(){
-				li.style.opacity = li.style.opacity/1 + 0.1;
-				li.style.paddingTop = parseInt(li.style.paddingTop) + 1 +'px';
-				li.style.paddingBottom = parseInt(li.style.paddingBottom) + 1 +'px';
-				if (li.style.opacity >= 0.9) {
-					clearInterval(showing)
-				}
-			}, 30)
-			return msg
-		})(msg)
-		
-	}
-	
+    GM_addStyle('#nice_alert {\
+      color: InfoText;\
+      font: 14px/16px sans-serif;\
+      position: fixed;\
+      top: 0;\
+      right: 0;\
+      margin: 0;\
+      padding: 0;\
+      list-style-type: none;\
+      float: left;\
+      cursor: pointer;\
+      text-align: left;\
+      z-index: 99;\
+    }\
+    #nice_alert li {\
+      background-color: InfoBackground;\
+      border-bottom: 1px solid #ccc;\
+      border-bottom: 1px solid rgba(0,0,0,0.3);\
+      margin: 0;\
+      opacity: 0;\
+      float: right;\
+      clear: both;\
+      overflow: hidden;\
+      font-size: 14px;\
+      outline: 0;\
+      -webkit-box-shadow: 0px 2px 8px rgba(0,0,0,0.2);\
+      -moz-box-shadow: 0px 2px 8px rgba(0,0,0,0.3);\
+    }');
+
+    var niceAlert = document.createElement('ol');
+    niceAlert.id = 'nice_alert';
+    
+    if (document.body) {
+      document.body.appendChild(niceAlert);
+      niceAlert.addEventListener('click',function(e){
+        var t = e.target;
+        if (t.tagName == 'LI') {
+          var h = t.clientHeight - 18;
+          t.style.height = h +'px';
+          var closing = setInterval(function(){
+            t.style.opacity -= 0.1;
+            //t.style.fontSize = parseInt(t.style.fontSize) - 1 +'px';
+            t.style.paddingTop = parseInt(t.style.paddingTop) - 1 +'px';
+            t.style.paddingBottom = parseInt(t.style.paddingBottom) - 1 +'px';
+            var currentHeight = parseInt(t.style.height) - h/10;
+            t.style.height = (currentHeight < 0 ? 0 : currentHeight) +'px';
+            if (t.style.opacity <= 0) {
+              t.style.display = 'none';
+              clearInterval(closing);
+            }
+          }, 30);
+        }
+      }, false);
+    }
+
+    var cache = document.createElement('li');
+    cache.style.padding = '0px 16px';
+    cache.tabIndex = 0;
+    
+    (w.alert = function(msg){
+      w.alert.isNice = 'Of couse it is!';
+      //if (niceAlert.childNodes.length > 99) return
+      var li = cache.cloneNode(false);
+      li.appendChild(document.createTextNode(msg));
+      niceAlert.appendChild(li);
+      var showing = setInterval(function(){
+        li.style.opacity = li.style.opacity/1 + 0.1;
+        li.style.paddingTop = parseInt(li.style.paddingTop) + 1 +'px';
+        li.style.paddingBottom = parseInt(li.style.paddingBottom) + 1 +'px';
+        if (li.style.opacity >= 0.9) {
+          clearInterval(showing);
+        }
+      }, 30);
+      return msg;
+    })(msg);
+    
+  };
+  
 }
 
-})()
+})();
