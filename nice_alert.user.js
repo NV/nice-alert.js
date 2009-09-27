52ce2f890c34781df4f9f8056543ff04290e2f4f
Nikita Vasilyev <me@elv1s.ru>
2009-09-27 23:42:40 +0400

    Now if script included double times it cause only one instanse of <ol id="nice_alert">

--- nice_alert.user.js
+++ nice_alert.user.js
@@ -4,16 +4,16 @@
 // @include     *
 // @description Not modal alert box
 // @author      Nikita Vasilyev
-// @version     0.7
+// @version     0.72
 // @licence     GPL
 // ==/UserScript==
 
 
 (function(){
 
-var w = window.wrappedJSObject || window;
+  var w = window.wrappedJSObject || window;
 
-if (!w.alert.is_nice) {
+  if (w.alert.is_nice) return;
 
   w.alert = function(msg){
 
@@ -62,31 +62,28 @@ if (!w.alert.is_nice) {
       -moz-box-shadow: 0px 2px 8px rgba(0,0,0,0.3);\
     }');
 
-    var nice_alert = document.createElement('ol');
+    var nice_alert = document.getElementById('nice_alert') || document.createElement('ol');
     nice_alert.id = 'nice_alert';
-    
-    if (document.body) {
-      document.body.appendChild(nice_alert);
-      nice_alert.addEventListener('click',function(e){
-        var t = e.target;
-        if (t.tagName == 'LI') {
-          var h = t.clientHeight - 18;
-          t.style.height = h +'px';
-          var closing = setInterval(function(){
-            t.style.opacity -= 0.1;
-            //t.style.fontSize = parseInt(t.style.fontSize) - 1 +'px';
-            t.style.paddingTop = parseInt(t.style.paddingTop) - 1 +'px';
-            t.style.paddingBottom = parseInt(t.style.paddingBottom) - 1 +'px';
-            var currentHeight = parseInt(t.style.height) - h/10;
-            t.style.height = (currentHeight < 0 ? 0 : currentHeight) +'px';
-            if (t.style.opacity <= 0) {
-              t.style.display = 'none';
-              clearInterval(closing);
-            }
-          }, 30);
-        }
-      }, false);
-    }
+    document.body.appendChild(nice_alert);
+    nice_alert.addEventListener('click',function(e){
+      var t = e.target;
+      if (t.tagName == 'LI') {
+        var h = t.clientHeight - 18;
+        t.style.height = h +'px';
+        var closing = setInterval(function(){
+          t.style.opacity -= 0.1;
+          //t.style.fontSize = parseInt(t.style.fontSize) - 1 +'px';
+          t.style.paddingTop = parseInt(t.style.paddingTop) - 1 +'px';
+          t.style.paddingBottom = parseInt(t.style.paddingBottom) - 1 +'px';
+          var currentHeight = parseInt(t.style.height) - h/10;
+          t.style.height = (currentHeight < 0 ? 0 : currentHeight) +'px';
+          if (t.style.opacity <= 0) {
+            t.style.display = 'none';
+            clearInterval(closing);
+          }
+        }, 30);
+      }
+    }, false);
 
     var cache = document.createElement('li');
     cache.style.padding = '0px 16px';
@@ -110,7 +107,5 @@ if (!w.alert.is_nice) {
     })(msg);
     
   };
-  
-}
 
 })();
