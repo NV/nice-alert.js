56650d97b64352d26cd5b70a5f0e9943a8677fd0
Nikita Vasilyev <me@elv1s.ru>
2009-12-22 02:10:02 +0300

    Metadata changed

--- nice_alert.user.js
+++ nice_alert.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Nice alert
-// @namespace   http://userscripts.ru/
+// @namespace   http://userscripts.ru/js/nice-alert/
 // @include     *
 // @description Makes alert box suck less
-// @author      Nikita Vasilyev
+// @copyright   2008+, Nikita Vasilyev (http://userscripts.org/scripts/show/43780)
 // @version     1.1
 // @licence     LGPL 3
 // ==/UserScript==
