b6f9c5d7ff1696f5aac0f648fa27d25c43df1111
Nikita Vasilyev <me@elv1s.ru>
2010-03-23 17:41:12 +0300

    Disable Nice Alert for XML documents Thanks to Ties for bug report https://chrome.google.com/extensions/detail/ehnbelnegmgdnjaghgomaakjcmpcakhk

--- chrome.js
+++ chrome.js
@@ -2,6 +2,8 @@ function main() {
 /*> nice_alert.js */
 }
 
-var script = document.createElement('script');
-script.appendChild(document.createTextNode('('+ main +')();'));
-document.documentElement.appendChild(script);
+if (!document.xmlVersion) {
+  var script = document.createElement('script');
+  script.appendChild(document.createTextNode('('+ main +')();'));
+  document.documentElement.appendChild(script);
+}
